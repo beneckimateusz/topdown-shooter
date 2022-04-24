55899cc3ce41115335bd12a0f050c6dd20f550a0
Mateusz Benecki <benecki.mateusz@gmail.com>
2022-04-24 19:50:15 +0200

    Improve player movement and add barrel controls (#3)

--- scenes/Player/Player.gd
+++ scenes/Player/Player.gd
@@ -1,26 +1,48 @@
 extends Area2D
 
-export var speed = 200 # (pixels/sec)
+signal health_changed
+signal dead
+
+export (PackedScene) var Bullet
+export (int) var speed = 200 # (pixels/sec)
+export (float) var rotation_speed = 1
+export (float) var gun_cooldown = 0.4
+export (int) var health = 100
+
 var screen_size
+var velocity
+var can_shoot = true
+var alive = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
+	$GunTimer.wait_time = gun_cooldown
+	
+func control(delta):
+	$Barrel.look_at(get_global_mouse_position())
+	
+	# tank rotation
+	var rotation_direction = 0
+	if Input.is_action_pressed("turn_right"):
+		rotation_direction += 1
+	if Input.is_action_pressed("turn_left"):
+		rotation_direction -= 1
+		
+	rotation += rotation_direction * rotation_speed * delta
+	
+	# tank movement
+	velocity = Vector2.ZERO
+	if Input.is_action_pressed("move_forward"):
+		velocity.x = speed
+	if Input.is_action_pressed("move_backward"):
+		velocity.x = -speed / 2
+		
+	velocity = velocity.rotated(rotation) * delta
 
 func _process(delta):
-	var velocity = Vector2.ZERO
-	if Input.is_action_pressed("move_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("move_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("move_down"):
-		velocity.y += 1
-	if Input.is_action_pressed("move_up"):
-		velocity.y -= 1
-
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
+	control(delta)
 		
-	position += velocity * delta
+	position += velocity
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
